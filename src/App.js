df1d1bc1a08ccbf20a03fa9cef0e606e5e068069
king-hiram <hirammuchiri6@gmail.com>
2022-05-04 15:28:38 +0300

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState }  from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  const [qoute, setQoute] = useState([]);
+
+  useEffect(() => {
+    let data = async () => {
+     let res = await  axios.get("http://api.quotable.io/random") 
+     setQoute(res.data)
+    }
+    data()
+  }, [])
+
+  let fetchData = async () => {
+    let res = await  axios.get("http://api.quotable.io/random") 
+    console.log(res);
+    setQoute(res.data)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='full'>
+    <div className="container content my-5">
+      <div className="row">
+        <div className="col-4">
+        <div className="card text-center">
+  <div className="card-body">
+    <h5 className="card-title">{qoute.author}</h5>
+    <p className="card-text">{qoute.content}</p>
+<button className="btn btn-primary" onClick={fetchData} >New Qoute</button>
+  </div>
+   </div>
+        </div>
+      </div>
+
     </div>
+   </div>
+    </>
   );
 }
 
